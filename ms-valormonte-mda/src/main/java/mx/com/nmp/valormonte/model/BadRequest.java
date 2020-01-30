f866b68b2828fa38db8f756db8e7b1fa4832ba57
JMartinez <rroramirez@gmail.com>
2020-01-30 13:51:02 -0600

    nuevo ms-valormonte-mda

---
+++ ms-valormonte-mda/src/main/java/mx/com/nmp/valormonte/model/BadRequest.java
@@ -0,0 +1,106 @@
+package mx.com.nmp.valormonte.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * BadRequest
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-01-30T17:34:52.695Z")
+
+public class BadRequest   {
+  @JsonProperty("codigo")
+  private String codigo = null;
+
+  @JsonProperty("mensaje")
+  private String mensaje = null;
+
+  public BadRequest codigo(String codigo) {
+    this.codigo = codigo;
+    return this;
+  }
+
+  /**
+   * Get codigo
+   * @return codigo
+  **/
+  @ApiModelProperty(example = "NMP-MDA-400", value = "")
+
+
+  public String getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
+  }
+
+  public BadRequest mensaje(String mensaje) {
+    this.mensaje = mensaje;
+    return this;
+  }
+
+  /**
+   * Get mensaje
+   * @return mensaje
+  **/
+  @ApiModelProperty(example = "El cuerpo de la petición no está bien formado, verifique su información.", value = "")
+
+
+  public String getMensaje() {
+    return mensaje;
+  }
+
+  public void setMensaje(String mensaje) {
+    this.mensaje = mensaje;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BadRequest badRequest = (BadRequest) o;
+    return Objects.equals(this.codigo, badRequest.codigo) &&
+        Objects.equals(this.mensaje, badRequest.mensaje);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(codigo, mensaje);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class BadRequest {\n");
+    
+    sb.append("    codigo: ").append(toIndentedString(codigo)).append("\n");
+    sb.append("    mensaje: ").append(toIndentedString(mensaje)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
