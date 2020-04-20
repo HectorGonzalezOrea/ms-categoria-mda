81078cc74d7569fb14e070f8b8cdf252ff53baa7
LhHernandez <46459046+LhHernandez@users.noreply.github.com>
2020-04-20 10:40:49 -0500

    Se agregan variables
    Se agregan las variables de entorno al request de enviar correo

--- ms-escenariosdinamicos-mda/src/main/java/mx/com/nmp/escenariosdinamicos/clienteservicios/service/ClienteCorreoService.java
+++ ms-escenariosdinamicos-mda/src/main/java/mx/com/nmp/escenariosdinamicos/clienteservicios/service/ClienteCorreoService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -29,6 +30,15 @@ public class ClienteCorreoService {
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	ConverterUtil convertJson= new ConverterUtil();
 	
+	@Value("${oag.resource.oauth.getToken.header.usuario}")
+	protected String headerUsuario;
+	
+	@Value("${oag.resource.oauth.getToken.header.idConsumidor}")
+	protected String headerIdConsumidor;
+	
+	@Value("${oag.resource.oauth.getToken.header.idDestino}")
+	protected String headerIdDestino;
+	
 	public RespuestaVO sendEmailUser(EnviarNotificacionRequestVO request) {
 		log.info("Entrando al metodo sendEmailUser ");
 		RespuestaVO respuesta=new RespuestaVO();
@@ -38,9 +48,9 @@ public class ClienteCorreoService {
 		HttpHeaders headers = new HttpHeaders();
 	
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.add(Common.HEADER_USUARIO, "nmp");
-		headers.add(Common.HEADER_ID_CONSUMIDOR, "46");
-		headers.add(Common.HEADER_ID_DESTINO, "100");
+		headers.add(Common.HEADER_USUARIO,headerUsuario);
+		headers.add(Common.HEADER_ID_CONSUMIDOR,headerIdConsumidor);
+		headers.add(Common.HEADER_ID_DESTINO, headerIdDestino);
 		headers.add(Common.HEADER_OAUTH_BEARER,token);
 		headers.setBasicAuth( Common.OAG_USUARIO, Common.OAG_PASSWORD);		
 		String requestJson = convertJson.messageToJson(request);
@@ -70,9 +80,9 @@ public class ClienteCorreoService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		headers.setBasicAuth( Common.OAG_USUARIO, Common.OAG_PASSWORD);
-		headers.add(Common.HEADER_USUARIO, "nmp");
-		headers.add(Common.HEADER_ID_CONSUMIDOR, "46");
-		headers.add(Common.HEADER_ID_DESTINO, "100");
+		headers.add(Common.HEADER_USUARIO,headerUsuario);
+		headers.add(Common.HEADER_ID_CONSUMIDOR,headerIdConsumidor);
+		headers.add(Common.HEADER_ID_DESTINO, headerIdDestino);
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
 	    params.add(Common.GRANT_TYPE, "client_credentials");
 	    params.add(Common.SCOPE, "UserProfile.me");
