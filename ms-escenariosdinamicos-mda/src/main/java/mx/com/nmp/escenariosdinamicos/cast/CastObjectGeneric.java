986298428d287f6ca0e7f450db8f331bc9bb9aa3
AlejandroAgRa <rroramirez18@gmail.com>
2020-05-07 16:44:00 -0500

    Se trabaja sobre indicentes
    Se trabaja sobre indicentes

--- ms-escenariosdinamicos-mda/src/main/java/mx/com/nmp/escenariosdinamicos/cast/CastObjectGeneric.java
+++ ms-escenariosdinamicos-mda/src/main/java/mx/com/nmp/escenariosdinamicos/cast/CastObjectGeneric.java
@@ -122,12 +122,9 @@ public class CastObjectGeneric {
 	}
 	
 	public List<PartidaVO> castPartidasToPartidaValorMonte(List<IndexGarantiaVO> lstResultServiceValorMonte,InfoRegla infoRegla){
-		List<PartidaVO> lstPartidas=null;
+		List<PartidaVO> lstPartidas=new ArrayList<>();
 		if (lstResultServiceValorMonte != null && !lstResultServiceValorMonte.isEmpty()) {
-			lstPartidas = new ArrayList<>();
-			for (IndexGarantiaVO entity : lstResultServiceValorMonte) {
-				lstPartidas.add(fillValues(entity,infoRegla));
-			}
+			lstResultServiceValorMonte.stream().forEach(entity->lstPartidas.add(fillValues(entity,infoRegla)));
 		}
 		return lstPartidas;
 	}
