5432f348e234dcf6983d7131b61c8f33d1b3e2ee
HctorGO <47544275+HctorGO@users.noreply.github.com>
2020-06-01 10:40:47 -0500

    Update Fix queue's

--- ms-escenariosdinamicos-mda/src/main/java/mx/com/nmp/escenariosdinamicos/model/component/ConsumerMessageComponent.java
+++ ms-escenariosdinamicos-mda/src/main/java/mx/com/nmp/escenariosdinamicos/model/component/ConsumerMessageComponent.java
@@ -1,23 +1,28 @@
 package mx.com.nmp.escenariosdinamicos.model.component;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
+import com.rabbitmq.client.Channel;
 
 import mx.com.nmp.escenariosdinamicos.cast.CastObjectGeneric;
+import com.rabbitmq.client.Channel;
 import mx.com.nmp.escenariosdinamicos.clienteoag.service.ClientOAGService;
 import mx.com.nmp.escenariosdinamicos.oag.dto.RequestReglaEscenarioDinamicoDto;
 import mx.com.nmp.escenariosdinamicos.oag.dto.ResponseOAGDto;
 import mx.com.nmp.escenariosdinamicos.oag.vo.PartidaResponseVO;
+import mx.com.nmp.escenariosdinamicos.oag.dto.RequestActualizarPrecioPartidaDto;
 import mx.com.nmp.escenariosdinamicos.oag.vo.PartidaVO;
-
 
 @Component
 public class ConsumerMessageComponent {
@@ -30,23 +35,45 @@ public class ConsumerMessageComponent {
 	CastObjectGeneric castObject;
 
 	@RabbitListener(queues = "${rabbitmq.queue}")
-	public void consumerReglaEscenarioDinamico(String message) {
+	@RabbitHandler
+	public void consumerReglaEscenarioDinamico( Channel channel, Message message) throws IOException{
 		log.info("Recieved Message==>consumerReglaEscenarioDinamico:: " + message);
-		clientOAG.reglaEscenarioDinamico(castObject.convertJsonToRequestEscenario(message));
+		
+		String mensaje= new String(message.getBody());
+		log.info("message EscenarioDinamico: {}", mensaje);
+		channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+		RequestReglaEscenarioDinamicoDto request=castObject.convertJsonToRequestEscenario(mensaje);
+		if(request !=null && !request.getPartida().isEmpty()) {
+			clientOAG.reglaEscenarioDinamico(request);
+		}
 	}
 	
 	@RabbitListener(queues = "${rabbitmq.queue.reglasarbitraje}")
-	public void consumerReglaArbitraje(String message) {
+	@RabbitHandler
+	public void consumerReglaArbitraje(Channel channel, Message message) throws IOException {
 		log.info("Recieved Message: " + message);
-		ResponseOAGDto messageVO=castObject.convertJsonToReponseOAGDto(message);
-		clientOAG.aplicarReglaArbitraje(convertObjectToJson(messageVO));
+		
+		String mensaje= new String(message.getBody());
+		log.info("message ReglaArbitraje {}", mensaje);
+		channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+		ResponseOAGDto messageVO = castObject.convertJsonToReponseOAGDto(mensaje);
+		if(messageVO !=null && !messageVO.getPartida().isEmpty()) {
+			clientOAG.aplicarReglaArbitraje(convertObjectToJson(messageVO));
+		}
 	}
 	
 	@RabbitListener(queues = "${rabbitmq.queue.cambioprecio}")
-	public void consumerCambioPrecio(String message) {
+	@RabbitHandler
+	public void consumerCambioPrecio(Channel channel, Message message) throws IOException {
 		log.info("Recieved Message: " + message);
-		//clientOAG.reglaEscenarioDinamico(castObject.convertJsonToRequestEscenario(message));
-		clientOAG.actualizarPrecioPartida(castObject.convertJsonToRequestActualizarPrecioPartida(message));
+		
+		String mensaje= new String(message.getBody());
+		log.info("message CambioPrecio {}", message);
+		channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+		RequestActualizarPrecioPartidaDto request=castObject.convertJsonToRequestActualizarPrecioPartida(mensaje);
+		if(request !=null) {
+			clientOAG.actualizarPrecioPartida(request);
+		}
 	}
 	
 	private RequestReglaEscenarioDinamicoDto  convertObjectToJson(ResponseOAGDto messageVO) {
@@ -66,10 +93,9 @@ public class ConsumerMessageComponent {
 	}
 	
 	
-	public RequestReglaEscenarioDinamicoDto convertJsonRquestEscenario(String message) {
+	private RequestReglaEscenarioDinamicoDto convertJsonRquestEscenario(String message) {
 		final Gson gson = new Gson();
-		RequestReglaEscenarioDinamicoDto request= new RequestReglaEscenarioDinamicoDto();
-		request=gson.fromJson(message, RequestReglaEscenarioDinamicoDto.class);
+		RequestReglaEscenarioDinamicoDto request  = gson.fromJson(message, RequestReglaEscenarioDinamicoDto.class);
 		return request;
 	}
 	
